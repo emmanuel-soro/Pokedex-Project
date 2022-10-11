5c2619c98039c26e8d12cafb32066cc3d1155c07
Emmanuel Soro <emmanuel.soro@gmail.com>
2022-10-10 22:20:20 -0300

    Added Spring Boot Reactive Api

---
+++ reactive-pokedex-api/src/main/java/com/frantic/dev/reactivepokedexapi/ReactivePokedexApiApplication.java
@@ -0,0 +1,13 @@
+package com.frantic.dev.reactivepokedexapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivePokedexApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivePokedexApiApplication.class, args);
+	}
+
+}
---
+++ reactive-pokedex-api/src/test/java/com/frantic/dev/reactivepokedexapi/ReactivePokedexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frantic.dev.reactivepokedexapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivePokedexApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
